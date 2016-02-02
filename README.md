4baa654d0f83265aeefd27b02fcf8cdc53216fb3
Camden Rudisill <camden.rudisill@richmond.edu>
2016-02-01 21:22:52 -0500

    Init Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PureTube
+
+This is a Youtube clone that is super basic, in a good way.
+No ugly comment section, no extra resources loaded, just a video player and a search bar.
+Built with React.
---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyDHHnqhK9Ik_9e7d7ywkN1dTBIBm63KyTc';
+
+
+// Create a component that creates HTML
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     }
+
+     this.videoSearch('rock climbing');
+
+
+  }
+
+  videoSearch (term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+
+// Take this compoent's generated HTML and
+// put it in the DOM
+
+ReactDOM.render(< App />, document.querySelector('.container'));
